a83dd06ce34f90aae398481549a44f5c5ab20511
Alperen21 <alperen.yildiz@u.nus.edu>
2024-07-03 17:10:21 +0300

    first commit

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n-seg.pt")  # load an official model
 
-results = model.train(data="coco8-seg.yaml", epochs=100, imgsz=640)
+results = model.train(data="train.yaml", epochs=1, imgsz=640)
